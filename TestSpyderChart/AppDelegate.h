c8ea41438f80a59bd47e2ba41d478d92299224d0
gireesh.boyina <gireesh.boyina@david.local>
2017-07-17 16:55:20 +0530

    Initial Commit

---
+++ TestSpyderChart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSpyderChart
+//
+//  Created by gireesh.boyina on 17/07/17.
+//  Copyright © 2017 gireesh.boyina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSpyderChart/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSpyderChart
+//
+//  Created by gireesh.boyina on 17/07/17.
+//  Copyright © 2017 gireesh.boyina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
